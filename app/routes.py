7f37710929a2b233e47d25388a7370c72ccd947d
cjcon90 <cjcon90@pm.me>
2021-02-21 12:56:34 +0000

    add user profile route

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app import app
 from app.forms import LoginForm, RegisterForm
 from flask_login import current_user, login_user, logout_user
 from app.models import User
+from random import randint
 
 
 @app.route('/')
@@ -19,6 +20,7 @@ def gallery():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """route for logging in users"""
     if current_user.is_authenticated:
         return redirect(url_for('gallery'))
     form = LoginForm()
@@ -42,6 +44,7 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """route to log out current user"""
     logout_user()
     return redirect(url_for('index'))
 
@@ -49,15 +52,19 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """route for registering new users"""
     if current_user.is_authenticated:
         return redirect(url_for('gallery'))
     form = RegisterForm()
     if request.method == 'POST' and form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
+        # Set a random avatar in case the user exits out of avatar select screen
+        user.set_avatar(f'https://res.cloudinary.com/cjcon90/image/upload/v1613871615/codeinstitute/hot_dogz/dog_avatars/dog{randint(1,16)}.png')
         user.save()
         login_user(user)
         flash("Registered! Please choose an avatar")
+        # Redirect to avatar select screen for manual select
         return redirect(url_for('select_avatar'))
     return render_template('register.html', title="Register", form=form)
 
@@ -65,11 +72,20 @@ def register():
 @app.route('/select_avatar')
 @login_required
 def select_avatar():
+    """route for selecting avatar for new users
+        or editing avatar for current users"""
     if request.args.get('selected'):
         user = User.objects.get(username=current_user.username)
         user.set_avatar(request.args.get('selected'))
         user.save()
         flash('Your avatar has been updated!')
-        return redirect(url_for('gallery'))
-    avatars = [f'https://res.cloudinary.com/cjcon90/image/upload/v1613871615/codeinstitute/hot_dogz/dog_avatars/dog{i}.jpg' for i in range(1,17)]
+        return redirect(url_for('profile', username=current_user.username))
+    avatars = [f'https://res.cloudinary.com/cjcon90/image/upload/v1613871615/codeinstitute/hot_dogz/dog_avatars/dog{i}.png' for i in range(1,17)]
     return render_template('select_avatar.html', avatars=avatars, title='Choose Avatar')
+
+
+@app.route('/profile/<username>')
+@login_required
+def profile(username):
+    user = User.objects.get(username=username)
+    return render_template('profile.html', user=user)
