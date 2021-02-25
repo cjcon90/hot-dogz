43d9add54f40ba8b199762079b4485f4f07e6ef2
cjcon90 <cjcon90@pm.me>
2021-02-25 18:35:30 +0000

    add smaller image thumbnail to dog model

--- app/models.py
+++ app/models.py
@@ -33,6 +33,7 @@ class Breed(db.Document):
 class Dog(db.Document):
     name = db.StringField(required=True)
     img_url = db.URLField(required=True)
+    img_url_thumb = db.URLField(required=True)
     owner = db.ReferenceField(User)
     breed = db.ReferenceField(Breed)
     about = db.StringField(default="No info on this doggo yet!")
@@ -47,16 +48,15 @@ class Dog(db.Document):
         cloud_name = os.environ.get("CLOUD_NAME")
         # add to URL for building URL
         endpoint = f"https://res.cloudinary.com/{cloud_name}/image/upload"
-        # Set the image automatically scaled to the maximum width available
-        # auto optimize image quality
-        # deliver in jpg
-        transformation = '/f_jpg,w_auto,c_scale,q_auto:low'
+        # Add transformations for delivering lower quality, smaller thumbnails
+        transformation = '/f_auto,w_500,c_scale,q_auto:low'
         # Get the version, id and format details from uploaded image
         version = f"/v{res['version']}/"
         public_id = res["public_id"]
         image_format = res["format"]
-        # compile above details to build full URL for uploaded image
-        self.img_url = f"{endpoint}{transformation}{version}{public_id}.{image_format}"
+        # add links for full quality image and thumbnails to Dog model
+        self.img_url = f"{endpoint}{version}{public_id}.{image_format}"
+        self.img_url_thumb = f"{endpoint}{transformation}{version}{public_id}.{image_format}"
         
 
 
