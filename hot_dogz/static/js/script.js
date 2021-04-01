09514bf3dadbfa81f177bfeec7a53e6739179682
cjcon90 <cjcon90@pm.me>
2021-04-01 17:05:12 +0100

    refactor script according to JShint warnings

--- hot_dogz/static/js/script.js
+++ hot_dogz/static/js/script.js
@@ -1,20 +1,21 @@
 // check if on gallery page by searching for main gallery container
-let galleryContainer = document.querySelector('.gallery-animate')
+let galleryContainer = document.querySelector('.gallery-animate');
 
 if(galleryContainer) {
   // if true, perform opacity animation on each child element
-  let dogCards = galleryContainer.children
+  let dogCards = galleryContainer.children;
   
   for (let i = 0; i < dogCards.length; i++){
-    fadeIn(i, dogCards[i])
+    fadeIn(i, dogCards[i]);
   }
-  
-  // Source on adding a delay within a loop: https://www.geeksforgeeks.org/how-to-add-a-delay-in-a-javascript-loop/
-  function fadeIn(i, card){
-    setTimeout(function() {
-      card.classList.remove('opacity-0')
-    }, 500 + (250 * i));
-  }
+}
+
+// Function for adding a delay within a loop
+// Souce: https://www.geeksforgeeks.org/how-to-add-a-delay-in-a-javascript-loop/
+function fadeIn(idx, e){
+  setTimeout(function() {
+    e.classList.remove('opacity-0');
+  }, 500 + (250 * idx));
 }
 
 // 'vh' unit fix for mobile browsers to prevent backgroung image resizing on browser bar appear/disappear
